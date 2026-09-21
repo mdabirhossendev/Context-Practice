62d7ec2283b42b42db28ff747446326dfdec7d1d
mdabirhossendev <md.abirhossendev0@gmail.com>
2026-09-21 21:50:27 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
 
         <div className="flex justify-center items-center h-screen">
           <div className="text-center rounded-sm shadow-2xl h-150 w-125 flex flex-col gap-10 justify-center items-center ">
+            <div></div>
             <CounterDisplay/>
             <CounterBtn />
             <CounterStatus />
